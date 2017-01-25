19ee4d96113f4fce77fc7b24738223b0bd0de333
frostkom <martin.fors@frostkom.se>
2017-01-25 13:42:50 +0100

    First commit

---
+++ include/functions.php
@@ -0,0 +1,156 @@
+<?php
+
+function activate_change_log()
+{
+	require_plugin("meta-box/meta-box.php", "Meta Box");
+}
+
+function post_updated_change_log($post_id, $post_after, $post_before)
+{
+	$arr_exclude = array('mf_log', 'mf_status_change', 'attachment', 'nav_menu_item', 'revision', 'template');
+
+	if(isset($post_after) && in_array($post_after->post_type, $arr_exclude))
+	{
+		return false;
+	}
+
+	else if(in_array(get_post_type($post_id), $arr_exclude))
+	{
+		return false;
+	}
+
+	else
+	{
+		$post_status = $post_status_before = $post_title = "";
+		
+		if(isset($post_after))
+		{
+			$post_status = $post_after->post_status;
+
+			if(isset($post_before))
+			{
+				$post_status_before = $post_before->post_status;
+
+				$post_title .= $post_status_before." -> ";
+			}
+
+			$post_title .= $post_status;
+		}
+
+		else
+		{
+			$post_status = get_post_status($post_id);
+
+			$post_title .= $post_status;
+		}
+
+		$post_title .= " (".date("Y-m-d H:i:s").")";
+
+		if($post_status != '' && $post_status != $post_status_before)
+		{
+			$post_data = array(
+				'post_type' => 'mf_status_change',
+				'post_parent' => $post_id,
+				'post_status' => $post_status,
+				'comment_status' => $post_status_before,
+				'post_title' => $post_title,
+			);
+
+			wp_insert_post($post_data);
+
+			//do_log(__FUNCTION__.": ".$post_id.($post_title != '' ? " (".$post_title.", ".get_current_user_id().")" : ""));
+		}
+	}
+}
+
+function before_delete_post_change_log($post_id)
+{
+	$user_id = get_current_user_id();
+
+	if($user_id > 0)
+	{
+		$user_data = get_userdata($user_id);
+		$user_name = $user_data->display_name;
+
+		$post_title = get_the_title($post_id);
+
+		do_log(sprintf(__("I just saw %s permanently delete '%s', is this alright?", 'lang_change_log'), $user_name, $post_title));
+	}
+}
+
+function get_status_changes()
+{
+	global $wpdb;
+
+	$out = "";
+
+	$post_id = filter_input(INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT);
+
+	if($post_id > 0)
+	{
+		$arr_statuses = array(
+			'auto-draft' => __("Auto-Draft", 'lang_change_log'),
+			'draft' => __("Draft", 'lang_change_log'),
+			'future' => __("Scheduled", 'lang_change_log'),
+			'pending' => __("Pending", 'lang_change_log'),
+			'publish' => __("Published", 'lang_change_log'),
+			'trash' => __("Trash", 'lang_change_log'),
+		);
+
+		$result = $wpdb->get_results($wpdb->prepare("SELECT post_author, post_date, post_status, comment_status FROM ".$wpdb->posts." WHERE post_type = 'mf_status_change' AND post_parent = '%d' ORDER BY post_date DESC", $post_id));
+
+		if($wpdb->num_rows > 0)
+		{
+			$out .= "<ul class='post-revisions'>";
+
+				foreach($result as $r)
+				{
+					$user_data = get_userdata($r->post_author);
+
+					$user_avatar = get_avatar($r->post_author, 24);
+					$user_name = $user_data->display_name;
+
+					$post_status_before = isset($arr_statuses[$r->comment_status]) ? $arr_statuses[$r->comment_status] : "";
+					$post_status = isset($arr_statuses[$r->post_status]) ? $arr_statuses[$r->post_status] : "";
+
+					$post_date = $r->post_date;
+
+					$out .= "<li>".$user_avatar." ".$user_name.", ".$post_status_before." -> ".$post_status." (".format_date($post_date).")</li>";
+				}
+
+			$out .= "</ul>";
+		}
+	}
+
+	if($out == "")
+	{
+		$out = "<em>".__("There are no status changes so far", 'lang_change_log')."</em>";
+	}
+
+	return $out;
+}
+
+function meta_boxes_change_log($meta_boxes)
+{
+	$meta_prefix = "mf_cl_";
+
+	if(current_user_can('manage_options')) //IS_ADMIN
+	{
+		$meta_boxes[] = array(
+			'id' => $meta_prefix.'status_changes',
+			'title' => __("Status Changes", 'lang_change_log'),
+			'pages' => array('page'),
+			//'context' => 'side',
+			'priority' => 'low',
+			'fields' => array(
+				array(
+					'id' => $meta_prefix.'status_changes',
+					'type' => 'custom_html',
+					'callback' => 'get_status_changes',
+				),
+			)
+		);
+	}
+
+	return $meta_boxes;
+}
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: MF Change Log
+Plugin URI: 
+Description: 
+Version: 1.2.1
+Author: Martin Fors
+Author URI: http://frostkom.se
+Text Domain: lang_change_log
+Domain Path: /lang
+
+GitHub Plugin URI: 
+*/
+
+include_once("include/functions.php");
+
+if(is_admin())
+{
+	register_activation_hook(__FILE__, 'activate_change_log');
+
+	add_action('post_updated', 'post_updated_change_log', 10, 3);
+	add_action('before_delete_post', 'before_delete_post_change_log');
+
+	add_action('rwmb_meta_boxes', 'meta_boxes_change_log');
+}
+
+load_plugin_textdomain('lang_change_log', false, dirname(plugin_basename(__FILE__)).'/lang/');
