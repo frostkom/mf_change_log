45fdc864c719e11651e4f2b11292e120eaa8bcb1
frostkom <martin.fors@frostkom.se>
2020-09-22 11:22:25 +0200

    Disable plugin if dependant plugins are not active

--- include/classes.php
+++ include/classes.php
@@ -1,2 +1,14 @@
 <?php
 
+class mf_change_log
+{
+	function __construct(){}
+
+	function admin_init()
+	{
+		if(!is_plugin_active("mf_base/index.php"))
+		{
+			deactivate_plugins(str_replace("include/classes.php", "index.php", plugin_basename(__FILE__)));
+		}
+	}
+}
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Change Log
 Plugin URI: 
 Description: 
-Version: 1.3.6
+Version: 1.3.7
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -14,20 +14,25 @@ Depends: Meta Box, MF Base
 GitHub Plugin URI: 
 */
 
+include_once("include/classes.php");
 include_once("include/functions.php");
+
+$obj_change_log = new mf_change_log();
 
 if(is_admin())
 {
+	register_activation_hook(__FILE__, 'activate_change_log');
+
 	add_action('init', 'init_change_log', 1);
 
-	register_activation_hook(__FILE__, 'activate_change_log');
+	add_action('admin_init', array($obj_change_log, 'admin_init'), 0);
 
 	add_action('post_updated', 'post_updated_change_log', 10, 3);
 
 	add_action('rwmb_meta_boxes', 'meta_boxes_change_log');
 }
 
-load_plugin_textdomain('lang_change_log', false, dirname(plugin_basename(__FILE__)).'/lang/');
+load_plugin_textdomain('lang_change_log', false, dirname(plugin_basename(__FILE__))."/lang/");
 
 function activate_change_log()
 {
